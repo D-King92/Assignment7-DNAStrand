29c0dd7969e51ac70d2c3493efedef48153eb00d
Daniel A. King <dking92@my.chemeketa.edu>
2026-02-20 19:49:12 +0000

    Feb 20, 2026, 11:49 AM

--- tests.cpp
+++ tests.cpp
@@ -191,19 +191,19 @@ TEST_CASE("getComplement")
   REQUIRE(isMatch2);
 }
 
-// TEST_CASE("operator+")
-// {
-//   cout << "8: Operator +..." << endl;
-//   DNAStrand str1("AATT");
-//   DNAStrand str2("CCGG");
-//   DNAStrand str3 = str1 + str2;
+TEST_CASE("operator+")
+{
+  cout << "8: Operator +..." << endl;
+  DNAStrand str1("AATT");
+  DNAStrand str2("CCGG");
+  DNAStrand str3 = str1 + str2;
 
-//   bool isMatch = (str3 == DNAStrand("AATTCCGG"));
-//   REQUIRE(isMatch);
+  bool isMatch = (str3 == DNAStrand("AATTCCGG"));
+  REQUIRE(isMatch);
 
-//   // Check originals aren't broken
-//   bool isMatch2 = (str1 == DNAStrand("AATT"));
-//   bool isMatch3 = (str2 == DNAStrand("CCGG"));
-//   REQUIRE(isMatch2);
-//   REQUIRE(isMatch3);
-// }
+  // Check originals aren't broken
+  bool isMatch2 = (str1 == DNAStrand("AATT"));
+  bool isMatch3 = (str2 == DNAStrand("CCGG"));
+  REQUIRE(isMatch2);
+  REQUIRE(isMatch3);
+}
